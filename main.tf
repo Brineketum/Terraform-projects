bb2c7ce72b2f2dab4e95bf8c1b2508f9775e2106
BRINE NDAM KETUM <113520986+Brineketum@users.noreply.github.com>
2023-03-04 13:26:37 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,50 +1,61 @@
-terraform {
-  backend "s3" {
-    bucket = "mybucketforterraformtesting718"
-    key    = "state/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#---/root---
 
-
-
+#create vpc
 module "vpc" {
-  source                  = "./vpc"
-  project_name            = var.project_name
-  vpc_cidr                = var.vpc_cidr
-  public_subnet1_az1_cidr = var.public_subnet1_az1_cidr
-  public_subnet2_az2_cidr = var.public_subnet2_az2_cidr
-
+  source                   = "./Modules/vpc"
+  project_name             = var.project_name
+  vpc_cidr                 = var.vpc_cidr
+  public_subnet1_az1_cidr  = var.public_subnet1_az1_cidr
+  public_subnet2_az2_cidr  = var.public_subnet2_az2_cidr
+  private_subnet1_az1_cidr = var.private_subnet1_az1_cidr
+  private_subnet2_az2_cidr = var.private_subnet2_az2_cidr
 
 }
 
 module "security-group" {
-  source = "./security-group"
+  source = "./Modules/security-group"
   vpc_id = module.vpc.vpc_id
-
-
 }
 
-module "asg" {
-  source             = "./asg"
+#create an ec2 instance
+module "Ec2" {
+  source             = "./Modules/ec2"
+  ami                = var.ami
   instance_type      = var.instance_type
-  imagge_id           = var.imagge_id
-  key_name           = var.key_name
-  public_subnet1_az1 = module.vpc.public_subnet1_az1
-  public_subnet1_az2 = module.vpc.public_subnet1_az2
-  load_balancer      = module.alb.load_balancer
-  target_group_arn   = module.alb.target_group_arn
-  project_name       = var.project_name
+  ec2_web_tag_name   = var.ec2_web_tag_name
+  keyname            = var.keyname
   ec2_security_group = module.security-group.ec2_security_group
+  subnet_ids         = module.vpc.subnet_ids
 
 }
+
+#create a RDS database instance
+
+module "database" {
+  source                  = "./Modules/database"
+  identifier              = var.identifier
+  engine                  = var.engine
+  engine_version          = var.engine_version
+  db_name                 = var.db_name
+  username                = var.username
+  password                = var.password
+  database-security-group = module.security-group.database-security-group
+  private_subnet1_az1     = module.vpc.private_subnet1_az1
+  private_subnet1_az2     = module.vpc.private_subnet1_az2
+
+}
+
+#create an application load balancer
 
 module "alb" {
-  source             = "./alb"
+  source             = "./Modules/alb"
   vpc_id             = module.vpc.vpc_id
-  subnet_ids         = module.vpc.subnet_ids
-  project_name       = var.project_name
   alb_security_group = module.security-group.alb_security_group
+  subnet_ids         = module.vpc.subnet_ids
+  aws_instance       = module.Ec2.aws_instance
+  project_name       = var.project_name
 
 }
-    
+
+
+
--- variables.tf
+++ variables.tf
@@ -1,39 +1,50 @@
+#---/root---
+
+#vpc
 variable "project_name" {
   type        = string
-  description = "This defines the bucket name for the project name"
+  description = "This configures the vpc name"
 }
-
 variable "vpc_cidr" {
   type        = string
-  description = "This defines the bucket name for the vpc cidr"
+  description = "This configures the vpc cidr"
 }
-
 variable "public_subnet1_az1_cidr" {
   type        = string
-  description = "This defines the cidr for subnet1"
+  description = "This configures the public subnet1 cidr"
 }
-
 variable "public_subnet2_az2_cidr" {
   type        = string
-  description = "This defines the cidr subnet2"
+  description = "This configures the public subnet2 cidr"
 }
-
-variable "instance_type" {
+variable "private_subnet1_az1_cidr" {
   type        = string
-  description = "This defines the instance type for the asg"
+  description = "This configures the private subnet1 cidr"
 }
-
-variable "imagge_id" {
-  
-  description = "This defines the image id for the asg"
-}
-
-variable "key_name" {
+variable "private_subnet2_az2_cidr" {
   type        = string
-  description = "This defines the existing keyname for the instance asg"
+  description = "This configures the private subnet2 cidr"
 }
 
-variable "region" {
-  type        = string
-  description = "This defines the region"
+#ec2
+variable "ami" {}
+variable "instance_type" {}
+variable "ec2_web_tag_name" {}
+variable "keyname" {}
+
+#database
+variable "identifier" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "db_name" {}
+variable "username" {
+  sensitive = true
 }
+variable "password" {
+  sensitive = true
+}
+
+
+
+
+
